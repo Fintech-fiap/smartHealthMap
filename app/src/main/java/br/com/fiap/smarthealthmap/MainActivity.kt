dbabdcdfdeb4ef7e31310aa5a002f926a7183847
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-03-18 12:22:14 -0300

    Feat:

--- app/src/main/java/br/com/fiap/smarthealthmap/MainActivity.kt
+++ app/src/main/java/br/com/fiap/smarthealthmap/MainActivity.kt
@@ -3,17 +3,26 @@ package br.com.fiap.smarthealthmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +46,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Home() {
-
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(180.dp, 220.dp),
+                painter = painterResource(id = R.drawable.home_blue_rectagle),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Detalhe da pagina"
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(280.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier.size(100.dp, 220.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo da smart health map"
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Image(
+                alignment = Alignment.BottomEnd,
+                modifier = Modifier.size(200.dp, 220.dp).offset(x = (10).dp),
+                painter = painterResource(id = R.drawable.home_red_rectagle),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Detalhe da pagina"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
