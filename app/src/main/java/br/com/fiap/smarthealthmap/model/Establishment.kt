28d5f7495971327d25119150320ae62d6f82eb86
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-03-19 02:17:14 -0300

    Feat: Add Establishment model

---
+++ app/src/main/java/br/com/fiap/smarthealthmap/model/Establishment.kt
@@ -0,0 +1,42 @@
+package br.com.fiap.smarthealthmap.model
+
+import com.google.gson.annotations.SerializedName
+
+ data class Establishment(
+     @SerializedName("nome_fantasia")
+     val name : String = "",
+     @SerializedName("codigo_uf")
+     val codigoUf : String = "",
+     @SerializedName("endereco_estabelecimento")
+     val logradouro : String = "",
+     @SerializedName("numero_estabelecimento")
+     val numerero : String = "",
+     @SerializedName("bairro_estabelecimento")
+     val bairro : String = "",
+     @SerializedName("codigo_cep_estabelecimento")
+     val cep : String = "",
+     @SerializedName("endereco_email_estabelecimento")
+     val email : String = "",
+     @SerializedName("numero_telefone_estabelecimento")
+     val telefone : String = "",
+     @SerializedName("descricao_turno_atendimento")
+     val turnos : String = "",
+     @SerializedName("latitude_estabelecimento_decimo_grau")
+     val latitude : String = "",
+     @SerializedName("longitude_estabelecimento_decimo_grau")
+     val longitude : String = "",
+     @SerializedName("estabelecimento_possui_centro_cirurgico")
+     val centroCirurgico : String = "",
+     @SerializedName("estabelecimento_possui_centro_obstetrico")
+     val centroObstetrico : String = "",
+     @SerializedName("estabelecimento_possui_centro_neonatal")
+     val centroNeonatal : String = "",
+     @SerializedName("estabelecimento_possui_atendimento_hospitalar")
+     val atendimentoHospitalar : String = "",
+     @SerializedName("estabelecimento_possui_servico_apoio")
+     val servicoApoio : String = "",
+     @SerializedName("estabelecimento_possui_atendimento_ambulatorial")
+     val atendimentoAmbulatorial : String = "",
+     @SerializedName("estabelecimento_faz_atendimento_ambulatorial_sus")
+     val atendimentoAmbulatorialSUS : String = "",
+ )
