93b4a8567bcf49d3ed897634a58799cd56fc7bc9
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-03-19 01:49:38 -0300

    Feat: Add Retrofit HTTP Client

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
 
     //Dependencia do Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // Dependências do Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
