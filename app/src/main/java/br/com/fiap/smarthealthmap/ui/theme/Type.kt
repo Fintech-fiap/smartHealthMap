19347b7edac7d6d5ca64f63565a6aab56e4d138c
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-03-26 20:20:54 -0300

    Style: Add FontFamily poppings_regular

--- app/src/main/java/br/com/fiap/smarthealthmap/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/smarthealthmap/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package br.com.fiap.smarthealthmap.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.smarthealthmap.R
+
+val poppings = FontFamily(Font(R.font.poppins_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
